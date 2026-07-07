5d8d01d02d509c5c0b799e683cbeee8b4b29682c
wkd48632 <wkd48632@gmail.com>
2026-07-07 20:34:41 +0900

    Update pygame_derivative01.py

--- pygame_derivative01.py
+++ pygame_derivative01.py
@@ -1,8 +1,28 @@
-import os
 # Hide the welcome message
+import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 
 import pygame
 import numpy
 
 screen = pygame.display.set_mode((0, 0), flags=pygame.FULLSCREEN)
+W, H = screen.get_size()
+
+# It’s going to be less than 60 times a second anyway, so why is it so verbose?
+# Let's return to the Zen of Python.
+def events():
+  raw = pygame.event.get()
+  ret = []
+  if event.type == pygame.QUIT:
+    ret.append(('quit',0))
+  elif event.type == pygame.KEYDOWN:
+    ret.append(('keydown'))
+  elif event.type == pygame.KEYUP:
+    ret.append(('keyup'))
+  elif event.type == pygame.MOUSEMOTION:
+    ret.append(('mousemove')) # refer to js
+  elif event.type == pygame.MOUSEBUTTONUP:
+    ret.append(('mouseup'))
+  elif event.type == pygame.MOUSSEBUTTONDDOWN:
+    ret.append(('mousedown'))
+
